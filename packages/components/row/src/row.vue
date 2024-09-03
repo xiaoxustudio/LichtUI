dde357666c664f58ea5e8d1ebb3dde5f78f2c5bb
xiaoxustudio <1783558957@qq.com>
2024-09-04 00:05:33 +0800

    remove vaild code

--- packages/components/row/src/row.vue
+++ packages/components/row/src/row.vue
@@ -1,9 +1,5 @@
 <template>
-	<div ref="rowRef" :class="[
-		bem.b(),
-		bem.is('row', direction === 'row'),
-		bem.is('column', direction === 'column'),
-	]" :style="{
+	<div :class="[bem.b(), bem.is('row', direction === 'row'), bem.is('column', direction === 'column'),]" :style="{
 		marginLeft: gutter ? `${gutter / 2}px` : '',
 		marginRight: gutter ? `${gutter / 2}px` : '',
 	}">
@@ -13,22 +9,10 @@
 <script setup lang="ts">
 	import { createNamespace } from "@licht-ui/utils/create";
 	import { rowProp } from "./row";
-	import { onMounted, ref } from "vue";
 	defineOptions({
 		name: "LiRow",
 	});
 	defineProps(rowProp);
 	const bem = createNamespace("row");
-	const rowRef = ref<HTMLDivElement>()
-	const bound = ref({})
-	let config = {
-		bound
-	}
-	defineExpose(config)
-	onMounted(() => {
-		if (rowRef.value) {
-			bound.value = rowRef.value.getBoundingClientRect()
-		}
-	})
 </script>
 <style scope lang="scss"></style>
