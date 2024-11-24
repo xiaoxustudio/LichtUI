165eab26419d27414d67eba6d25864283d3a0a4a
xiaoxustudio <1783558957@qq.com>
2024-11-24 11:29:21 +0800

    update: docs build bug

--- packages/components/message/src/useMessage.ts
+++ packages/components/message/src/useMessage.ts
@@ -1,21 +1,27 @@
 import { createApp, ref } from "vue";
 import { MessageEX, MessageP } from "./message";
 import { uuid } from "@licht-ui/utils/src/uuid";
-
 import messageContainer from "./message-container.vue";
-const div = document.createElement("div");
-div.classList.add("licht-overlayer");
-document.body.appendChild(div);
+
 const messageQueue = ref<MessageEX[]>([]);
-createApp(messageContainer, {
-	queue: messageQueue,
-}).mount(div);
+const initVal = ref(false);
+
+const init = () => {
+	const div = document.createElement("div");
+	div.classList.add("licht-overlayer");
+	document.body.appendChild(div);
+	createApp(messageContainer, {
+		queue: messageQueue,
+	}).mount(div);
+	initVal.value = true;
+};
 
 export const remove = (index: number) => {
 	messageQueue.value.splice(index, 1);
 };
 
 export const message = (prop: MessageP) => {
+	!initVal.value && init();
 	const _uuid = uuid();
 	let _msg = {
 		...prop,
